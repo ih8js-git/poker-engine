01045f05d42b6b2fa09f2d942aa86c8645c0cb29
ih8js-git <141177946+ih8js-git@users.noreply.github.com>
2026-05-09 22:38:36 -0500

    feat: add player structs, table positions, and card shuffling functionality using the rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rand;
+use rand::prelude::SliceRandom;
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Suits {
     Hearts,
@@ -31,6 +34,37 @@ pub struct Card {
 
 pub type Deck = Vec<Card>;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum TablePosition {
+    Button, // Also known as the Dealer
+    SmallBlind,
+    BigBlind,
+    Standard,
+}
+
+#[derive(Debug)]
+pub struct Player {
+    pub hand: Vec<Card>,
+    pub name: String,
+    pub position: TablePosition,
+    pub chips: u32,
+}
+
+impl Player {
+    pub fn new(name: String, position: TablePosition, chips: u32) -> Player {
+        return Player {
+            hand: Vec::with_capacity(2),
+            name,
+            position,
+            chips,
+        };
+    }
+}
+
+fn deal(deck: &mut Deck, players: &mut Vec<Player>) {
+    for player in players {}
+}
+
 impl Card {
     pub fn new_deck() -> Deck {
         let mut deck = Vec::with_capacity(52);
@@ -49,7 +83,28 @@ impl Card {
 }
 
 fn main() {
-    println!("Hello, world!");
-    let deck = Card::new_deck();
-    println!("{:?}", deck);
+    let mut rng = rand::rng();
+    let mut deck = Card::new_deck();
+    let number_of_players = rand::random_range(3..=6);
+    let mut players: Vec<Player> = vec![];
+
+    for player_number in 1..=number_of_players {
+        let position = match player_number {
+            1 => TablePosition::Button,
+            2 => TablePosition::SmallBlind,
+            3 => TablePosition::BigBlind,
+            _ => TablePosition::Standard,
+        };
+        players.push(Player::new(
+            format!("Player {}", player_number),
+            position,
+            1000,
+        ));
+    }
+
+    println!("Dealing cards to {:?}", &players);
+
+    deck.shuffle(&mut rng);
+
+    deal(&mut deck, &mut players);
 }
