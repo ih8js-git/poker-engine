d6ae18ecc2cb6e1842b0aa696e985c65f87a671e
ih8js-git <141177946+ih8js-git@users.noreply.github.com>
2026-05-09 22:01:43 -0500

    feat: implement card, suit, and rank data structures with deck generation logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Suits {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Ranks {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Card {
+    pub rank: Ranks,
+    pub suit: Suits,
+}
+
+pub type Deck = Vec<Card>;
+
+impl Card {
+    pub fn new_deck() -> Deck {
+        let mut deck = Vec::with_capacity(52);
+        use Ranks::*;
+        use Suits::*;
+
+        for suit in [Hearts, Diamonds, Clubs, Spades] {
+            for rank in [
+                Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace,
+            ] {
+                deck.push(Card { rank, suit });
+            }
+        }
+        return deck;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let deck = Card::new_deck();
+    println!("{:?}", deck);
 }
